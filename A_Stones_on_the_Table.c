c9a3478870618722ee15fee535dca94f0838527b
AYAN10M <gopalchandrahaldar1997@gmail.com>
2023-12-17 20:25:19 +0530

    stones on the tablet

---
+++ A_Stones_on_the_Table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    // number of stone
+    int n;
+    scanf("%d",&n);
+
+    // array where we will store the data of stone colors
+    char s[n+1];
+    scanf("%s", s);
+
+
+    printf("Hello World");
+    return 0;
+}
