293e187aeb7d1196e57adaebd1a647006f5b6dde
AYAN10M <gopalchandrahaldar1997@gmail.com>
2023-12-18 20:02:39 +0530

    stones_on_the_table

--- A_Stones_on_the_Table.c
+++ A_Stones_on_the_Table.c
@@ -5,13 +5,26 @@ int main()
 {
     // number of stone
     int n;
-    scanf("%d",&n);
-
+    scanf("%d", &n);
+ 
     // array where we will store the data of stone colors
-    char s[n+1];
+    char s[n + 1];
     scanf("%s", s);
-
-
-    printf("Hello World");
+    int i, j, count = 0;
+    for (i = 0; i < strlen(s); i++)
+    {
+        if (s[i - 1] == s[i])
+        {
+            count++;
+        }
+        else
+        {
+            continue;
+        }
+        
+        
+    }
+ 
+    printf("%d", count);
     return 0;
 }
